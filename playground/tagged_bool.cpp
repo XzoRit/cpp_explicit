dc456ef620c7d551270a291791b39766e78060af
XzoRit <xzorit@hotmail.com>
2017-12-31 14:03:28 +0100

    use proxy doctest header and its test macros

--- playground/tagged_bool.cpp
+++ playground/tagged_bool.cpp
@@ -1,5 +1,5 @@
+#include "doctest_proxy.hpp"
 #include <ak_toolkit/tagged_bool.hpp>
-#include <doctest.h>
 #include <string>
 
 namespace xplicit = ak_toolkit::xplicit;
@@ -20,32 +20,32 @@ void make_file(readonly _readonly, shared _shared)
     else param_spy += "ns";
 }
 
-TEST_CASE("tagged_bool")
+test_case("tagged_bool")
 {
     param_spy.clear();
 
-    SUBCASE("readonly, shared")
+    subcase("readonly, shared")
     {
         make_file(readonly(true), shared(true));
 
-        CHECK(param_spy == string{"rs"});
+        check_eq(param_spy, string{"rs"});
     }
-    SUBCASE("!readonly, shared")
+    subcase("!readonly, shared")
     {
         make_file(readonly(false), shared(true));
 
-        CHECK(param_spy == string{"rws"});
+        check_eq(param_spy, string{"rws"});
     }
-    SUBCASE("readonly, !shared")
+    subcase("readonly, !shared")
     {
         make_file(readonly(true), shared(false));
 
-        CHECK(param_spy == string{"rns"});
+        check_eq(param_spy, string{"rns"});
     }
-    SUBCASE("!readonly, !shared")
+    subcase("!readonly, !shared")
     {
         make_file(readonly(false), shared(false));
 
-        CHECK(param_spy == string{"rwns"});
+        check_eq(param_spy, string{"rwns"});
     }
 }
