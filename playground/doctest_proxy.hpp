8d227c53ed968467feb0b4f52504188fc85d1c86
XzoRit <xzorit@hotmail.com>
2017-12-31 14:01:39 +0100

    make proxy header to easily change the type of assertion macros
    Doctest offers a variaty of "fast-asserts" which can be configured
    at compile time. To not always search and replace all the macros
    a proxy header has been introduced.

---
+++ playground/doctest_proxy.hpp
@@ -0,0 +1,7 @@
+#define DOCTEST_CONFIG_NO_SHORT_MACROS
+#define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
+#include <doctest.h>
+
+#define test_case DOCTEST_TEST_CASE
+#define subcase DOCTEST_SUBCASE
+#define check_eq DOCTEST_FAST_CHECK_EQ
