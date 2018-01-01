2324f2783121e23e044cebeb15c3b3c80b914157
XzoRit <xzorit@hotmail.com>
2018-01-01 10:06:15 +0100

    add suite macros

--- playground/doctest_proxy.hpp
+++ playground/doctest_proxy.hpp
@@ -1,8 +1,12 @@
 #define DOCTEST_CONFIG_COLORS_NONE
 #define DOCTEST_CONFIG_NO_SHORT_MACROS
+#define DOCTEST_CONFIG_NO_TRY_CATCH_IN_ASSERTS
 #define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
 #include <doctest.h>
 
-#define check_eq  DOCTEST_FAST_CHECK_EQ
-#define subcase   DOCTEST_SUBCASE
-#define test_case DOCTEST_TEST_CASE
+#define check_eq    DOCTEST_FAST_CHECK_EQ
+#define subcase     DOCTEST_SUBCASE
+#define suite_begin DOCTEST_TEST_SUITE_BEGIN
+#define suite_end   DOCTEST_TEST_SUITE_END
+#define test_case   DOCTEST_TEST_CASE
+
