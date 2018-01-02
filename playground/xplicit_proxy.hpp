3b4fd42a06a41777ee4a58119d1a82abe31a1606
XzoRit <xzorit@hotmail.com>
2018-01-02 19:56:56 +0100

    add out_param related symbols to xplicit proxy header

--- playground/xplicit_proxy.hpp
+++ playground/xplicit_proxy.hpp
@@ -1,4 +1,5 @@
 #include <ak_toolkit/not_null.hpp>
+#include <ak_toolkit/out_param.hpp>
 #include <ak_toolkit/tagged_bool.hpp>
 
 namespace xplicit = ak_toolkit::xplicit;
@@ -6,3 +7,5 @@ namespace xplicit = ak_toolkit::xplicit;
 using xplicit::as_not_null;
 using xplicit::not_null;
 using xplicit::tagged_bool;
+using xplicit::out_param;
+using xplicit::out;
