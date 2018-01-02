6843683df3d0ae13d9a78d51409556a4dabd5841
XzoRit <xzorit@hotmail.com>
2018-01-02 17:27:15 +0100

    show that not_null can be used for unique and shared pointer too

--- playground/not_null.cpp
+++ playground/not_null.cpp
@@ -1,11 +1,24 @@
 #include "doctest_proxy.hpp"
 #include "xplicit_proxy.hpp"
+#include <memory>
 
 namespace
 {
+    using namespace std;
+
     int param_spy{};
 
-    void need_raw_pointer(not_null<int*> a)
+    void need_pointer(not_null<int*> a)
+    {
+        param_spy = *a;
+    }
+
+    void need_pointer(not_null<unique_ptr<int>> a)
+    {
+        param_spy = *a;
+    }
+
+    void need_pointer(not_null<shared_ptr<int>> a)
     {
         param_spy = *a;
     }
@@ -15,12 +28,20 @@ suite_begin("not_null");
 
 test_case("raw pointer")
 {
-    need_raw_pointer(as_not_null(new int{1}));
+    need_pointer(as_not_null(new int{1}));
     check_eq(param_spy, 1);
 }
 
-test_case("2")
+test_case("unique pointer")
 {
+    need_pointer(as_not_null(std::make_unique<int>(2)));
+    check_eq(param_spy, 2);
+}
+
+test_case("shared pointer")
+{
+    need_pointer(as_not_null(std::make_shared<int>(3)));
+    check_eq(param_spy, 3);
 }
 
 suite_end();
